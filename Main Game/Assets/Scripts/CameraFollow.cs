20561cd09f650e3cdcf583f130e3b0bbc52b912b
Fazekas <fazekas.nick101@gmail.com>
2017-07-31 22:52:16 -0400

    camera can rotate
    Problem with movement after rotations

---
+++ Main Game/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset;
+	public float horizontalSpeed;
+	private Vector3 rotation;
+    
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+
+		rotation = new Vector3(0, offset.y, offset.z);
+    }
+    
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;   
+
+		rotation = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * horizontalSpeed, Vector3.up) * rotation;
+
+		transform.position = player.transform.position + rotation;
+		transform.LookAt(player.transform.position);
+
+	}
+		
+}
